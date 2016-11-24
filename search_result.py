9e0df7f771b9e58a1f6f5324f0bc986a7512db72
Vincenzo Cerminara <vincenzo.cerminara94@gmail.com>
2016-11-24 23:48:00 +0100

    now using Gio to get the symlink of uncertain file

--- search_result.py
+++ search_result.py
@@ -17,7 +17,13 @@ class SearchResult():
 			self.size = -1
 		self.file_name = os.path.split(full_path)[1]
 
-		self.mimetype = Gio.content_type_get_mime_type(Gio.content_type_guess(filename=full_path, data=None)[0])
+		guessed_content_type = Gio.content_type_guess(filename=full_path, data=None)
+		if not guessed_content_type[1]: #uncertain is false
+			self.mimetype = Gio.content_type_get_mime_type(guessed_content_type[0])
+		else:
+			f = Gio.file_parse_name(file_path)
+			f_info = f.query_info('standard::content-type', flags=Gio.FileQueryInfoFlags.NOFOLLOW_SYMLINKS, cancellable=None)
+			self.mimetype = f_info.get_content_type()
 
 	# alternative content_type, val = Gio.content_type_guess('filename=foo.pdf', data=None)
 
